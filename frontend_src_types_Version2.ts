18cce9234129881f1833f630835c1c8c3611da3a
Lazaro Gomez Vitolo <lazarogvitolo@gmail.com>
2026-01-01 09:22:07 -0300

    Delete frontend_src_types_Version2.ts

--- frontend_src_types_Version2.ts
+++
@@ -1,9 +0,0 @@
-export interface Product {
-  id: string;
-  title: string;
-  price: number;
-  thumbnail?: string;
-  link?: string;
-  // Optional field for model prediction / score
-  predicted_score?: number;
-}
